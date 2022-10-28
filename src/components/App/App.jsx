05406bab6425e47fffa09db6db70f51c7c3c2f48
Noelani West <noelani.rose14@gmail.com>
2022-10-27 20:03:09 -0500

    set up index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+// IMPORT COMPONENTS
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+  }, [])
+
+  // AXIOS POST
+  // take in feedback as argument 
+  // function to submit feedback and send to database
+  // then clear feedback object 
 
   return (
+
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
