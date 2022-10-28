78b68cd612e821358c0d9421e6dd81130aa69750
Noelani West <noelani.rose14@gmail.com>
2022-10-27 19:48:45 -0500

    set up feedback.router.js

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const pool = require('../modules/pool')
+const router = new express.Router();
+
+// I don't think I need this because I'm not displaying all?
+// But I want to display most recent on the Review Page? 
+
+// router.get('/', (req, res) => {
+//     pool.query('SELECT * FROM "feedback";')
+
+//     .then((res) => {
+//         res.send(res.rows)
+//     })
+//     .catch((err) => {
+//         console.log('error GETting feedback', err);
+//         res.sendStatus(500);
+//     })
+// })
+
+router.post('/', (req, res) => {
+    pool.query('')
+
+    .then((res) => {
+        res.send(res.rows) 
+    })
+    .catch((err) => {
+        console.log('error POSTing feedback', err)
+        res.sendStatus(500);
+    })
+})
